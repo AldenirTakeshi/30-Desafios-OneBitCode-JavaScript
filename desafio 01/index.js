70fcd73a73858a89ede40d9bf1bebe8d94749c5d
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-11-06 13:25:20 -0400

    First commit

---
+++ desafio 01/index.js
@@ -0,0 +1,16 @@
+function media(numbers) {
+  let total = 0;
+  numbers.forEach((number) => {
+    total += number;
+    return total;
+  });
+
+  const media = total / numbers.length;
+
+  return media;
+}
+
+console.log(media([10, 9, 6, 8, 9, 1, 5, 7]));
+console.log(media([2, 5, 7, 1, -2]));
+console.log(media([10, 10, 10, 10, 9]));
+console.log(media([25, 75]));
